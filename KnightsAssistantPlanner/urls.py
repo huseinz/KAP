f9d82341d5fee62098ddc4233103fde14a89a247
eric howard <domhoward14@gmail.com>
2015-10-11 23:17:23 -0400

    Calendar is dynamically making the interface for all months and years

--- KnightsAssistantPlanner/urls.py
+++ KnightsAssistantPlanner/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns('',
               url(r'^$', views.index, name='index'),
               url(r'^newspage|^newsPage|^NewsPage', views.newsPage, name='newsPage'),
               url(r'^myhealth|^Myhealth|^MyHealth', views.myHealth, name='myHealth'),
-              url(r'Calendar|calendar', views.Calendar, name='Calendar'),
+              url(r'^myCalendar|^mycalendar', views.CalendarNp, name='CalendarNp'),
+              url(r'^(Calendar|calendar)/(?P<Date>[\w\-]+)/$', views.Calendar, name='Calendar'),
+              url(r'^Calendar|^calendar', views.CalendarNp, name='CalendarNp'),
               )
--- KnightsAssistantPlanner/views.py
+++ KnightsAssistantPlanner/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime, string, calendar
 
 # Create your views here
 def index (request):
@@ -11,5 +12,105 @@ def newsPage (request):
 def myHealth (request):
     return render(request, 'myHealth.html')
 
-def Calendar (request):
-    return render(request, 'calendar.html')
+def CalendarNp(request):
+    currentDay = datetime.date.today().strftime("%d")
+    year = int(datetime.date.today().strftime("%y"))
+    monthNumber = datetime.date.today().strftime("%m")
+    month = getMonthProperties(monthNumber,year)[2]
+    fstDayOfMonth = int(calendar.monthrange(year,int(monthNumber))[0])
+    secondWkSp = 7 - fstDayOfMonth
+    daysInMonth = calendar.monthrange(year,int(monthNumber))[1]
+    daysInMonth = range(daysInMonth)[secondWkSp:]
+    startingPoints = [secondWkSp, secondWkSp+7, secondWkSp+14, secondWkSp+21]
+    endingPoints = [secondWkSp+6, secondWkSp+13, secondWkSp+20, secondWkSp+27]
+    context_dictionary = {'month':month, 'year':year, 'currentDay':currentDay, 'fstDayOfMonth':range(fstDayOfMonth+1), 'daysInMonth':daysInMonth}
+    context_dictionary['leftOver'] = range(secondWkSp)[1:]
+    context_dictionary['startingPoints'] = startingPoints
+    context_dictionary['endingPoints'] = endingPoints
+    return render(request, 'calendar.html', context_dictionary)
+
+def Calendar (request, Date):
+    monthNumber = int(string.split(Date, "-")[0])
+    year = int(string.split(Date, "-")[1])
+    month = getMonthProperties(monthNumber,year)[2]
+    fstDayOfMonth = int(calendar.monthrange(year,int(monthNumber))[0])
+    secondWkSp = 7 - fstDayOfMonth
+    daysInMonth = calendar.monthrange(year,int(monthNumber))[1]
+    daysInMonth = range(daysInMonth)[secondWkSp:]
+    startingPoints = [secondWkSp, secondWkSp+7, secondWkSp+14, secondWkSp+21]
+    endingPoints = [secondWkSp+6, secondWkSp+13, secondWkSp+20, secondWkSp+27]
+    context_dictionary = {'month':month, 'year':year, 'fstDayOfMonth':range(fstDayOfMonth+1), 'daysInMonth':daysInMonth}
+    context_dictionary['leftOver'] = range(secondWkSp)[1:]
+    context_dictionary['startingPoints'] = startingPoints
+    context_dictionary['endingPoints'] = endingPoints
+    return render(request, 'calendar.html', context_dictionary)
+
+#This function outputs the 1st day of the month
+#Number of days for that month and the month itself
+#all of which are needed to make the
+def getMonthProperties(monthNumber, year):
+    if monthNumber == "1":
+        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        month = "January"
+        return(fstDayAndNumOfDays, month)
+
+    if monthNumber == "2":
+        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        month = "Feburary"
+        return(fstDayAndNumOfDays, month)
+
+    if monthNumber == "3":
+        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        month = "March"
+        return(fstDayAndNumOfDays, month)
+
+    if monthNumber == "4":
+        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        month = "April"
+        return(fstDayAndNumOfDays, month)
+
+    if monthNumber == "5":
+        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        month = "May"
+        return(fstDayAndNumOfDays, month)
+
+    if monthNumber == "6":
+        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        month = "June"
+        return(fstDayAndNumOfDays, month)
+
+    if monthNumber == "7":
+        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        month = "July"
+        return(fstDayAndNumOfDays, month)
+
+    if monthNumber == "8":
+        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        month = "August"
+        return(fstDayAndNumOfDays, month)
+
+    if monthNumber == "9":
+        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        month = "September"
+        return(fstDayAndNumOfDays, month)
+
+    if monthNumber == "10":
+        fstDayAndNumOfDays = calendar.monthrange(year,10)
+        month = "October"
+        return fstDayAndNumOfDays + (month,)
+
+    if monthNumber == "11":
+        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        month = "November"
+        return(fstDayAndNumOfDays, month)
+
+    if monthNumber == "12":
+        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        month = "December"
+        return(fstDayAndNumOfDays, month)
+
+    else:
+        return "Undefined"
+
+
+
