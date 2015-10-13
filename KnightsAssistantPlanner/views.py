5dd0e2efa32d029612735f234afecdc20ce2c1ec
eric howard <domhoward14@gmail.com>
2015-10-13 17:17:04 -0400

    added some quick javascript to the interface

--- KnightsAssistantPlanner/views.py
+++ KnightsAssistantPlanner/views.py
@@ -32,14 +32,14 @@ def CalendarNp(request):
 def Calendar (request, Date):
     monthNumber = int(string.split(Date, "-")[0])
     year = int(string.split(Date, "-")[1])
-    month = getMonthProperties(monthNumber,year)[2]
+    month = getMonthProperties(str(monthNumber),year)[2]
     fstDayOfMonth = int(calendar.monthrange(year,int(monthNumber))[0])
     secondWkSp = 7 - fstDayOfMonth
     daysInMonth = calendar.monthrange(year,int(monthNumber))[1]
-    daysInMonth = range(daysInMonth)[secondWkSp:]
+    daysInMonth = range(daysInMonth + 1)[secondWkSp:]
     startingPoints = [secondWkSp, secondWkSp+7, secondWkSp+14, secondWkSp+21]
     endingPoints = [secondWkSp+6, secondWkSp+13, secondWkSp+20, secondWkSp+27]
-    context_dictionary = {'month':month, 'year':year, 'fstDayOfMonth':range(fstDayOfMonth+1), 'daysInMonth':daysInMonth}
+    context_dictionary = {'month':month, 'year':year, 'fstDayOfMonth':range(fstDayOfMonth+1), 'daysInMonth':daysInMonth }
     context_dictionary['leftOver'] = range(secondWkSp)[1:]
     context_dictionary['startingPoints'] = startingPoints
     context_dictionary['endingPoints'] = endingPoints
@@ -50,64 +50,64 @@ def Calendar (request, Date):
 #all of which are needed to make the
 def getMonthProperties(monthNumber, year):
     if monthNumber == "1":
-        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        fstDayAndNumOfDays = calendar.monthrange(year,int(monthNumber))
         month = "January"
-        return(fstDayAndNumOfDays, month)
+        return fstDayAndNumOfDays + (month,)
 
     if monthNumber == "2":
-        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        fstDayAndNumOfDays = calendar.monthrange(year,int(monthNumber))
         month = "Feburary"
-        return(fstDayAndNumOfDays, month)
+        return fstDayAndNumOfDays + (month,)
 
     if monthNumber == "3":
-        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        fstDayAndNumOfDays = calendar.monthrange(year,int(monthNumber))
         month = "March"
-        return(fstDayAndNumOfDays, month)
+        return fstDayAndNumOfDays + (month,)
 
     if monthNumber == "4":
-        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        fstDayAndNumOfDays = calendar.monthrange(year,int(monthNumber))
         month = "April"
-        return(fstDayAndNumOfDays, month)
+        return fstDayAndNumOfDays + (month,)
 
     if monthNumber == "5":
-        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        fstDayAndNumOfDays = calendar.monthrange(year,int(monthNumber))
         month = "May"
-        return(fstDayAndNumOfDays, month)
+        return fstDayAndNumOfDays + (month,)
 
     if monthNumber == "6":
-        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        fstDayAndNumOfDays = calendar.monthrange(year,int(monthNumber))
         month = "June"
-        return(fstDayAndNumOfDays, month)
+        return fstDayAndNumOfDays + (month,)
 
     if monthNumber == "7":
-        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        fstDayAndNumOfDays = calendar.monthrange(year,int(monthNumber))
         month = "July"
-        return(fstDayAndNumOfDays, month)
+        return fstDayAndNumOfDays + (month,)
 
     if monthNumber == "8":
-        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        fstDayAndNumOfDays = calendar.monthrange(year,int(monthNumber))
         month = "August"
-        return(fstDayAndNumOfDays, month)
+        return fstDayAndNumOfDays + (month,)
 
     if monthNumber == "9":
-        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        fstDayAndNumOfDays = calendar.monthrange(year,int(monthNumber))
         month = "September"
-        return(fstDayAndNumOfDays, month)
+        return fstDayAndNumOfDays + (month,)
 
     if monthNumber == "10":
-        fstDayAndNumOfDays = calendar.monthrange(year,10)
+        fstDayAndNumOfDays = calendar.monthrange(year,int(monthNumber))
         month = "October"
         return fstDayAndNumOfDays + (month,)
 
     if monthNumber == "11":
-        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        fstDayAndNumOfDays = calendar.monthrange(year,int(monthNumber))
         month = "November"
-        return(fstDayAndNumOfDays, month)
+        return fstDayAndNumOfDays + (month,)
 
     if monthNumber == "12":
-        fstDayAndNumOfDays = calendar.monthrange(year,monthNumber)
+        fstDayAndNumOfDays = calendar.monthrange(year,int(monthNumber))
         month = "December"
-        return(fstDayAndNumOfDays, month)
+        return fstDayAndNumOfDays + (month,)
 
     else:
         return "Undefined"
