3db5e50e7b217f313bbe2d86b05817046113cb3b
HuseinZ <zubirhusein@gmail.com>
2015-11-21 23:54:04 -0500

    finally got this working

--- KnightsAssistantPlanner/migrations/0001_initial.py
+++ KnightsAssistantPlanner/migrations/0001_initial.py
@@ -1,32 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
-<<<<<<< HEAD
-from django.conf import settings
-=======
->>>>>>> 8da517b39a1f3428cbb0342a2083e8ec1e0cda8b
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-<<<<<<< HEAD
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-=======
->>>>>>> 8da517b39a1f3428cbb0342a2083e8ec1e0cda8b
     ]
 
     operations = [
         migrations.CreateModel(
-<<<<<<< HEAD
-            name='UserProfile',
-            fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
-                ('website', models.URLField(blank=True)),
-                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
-            ],
-=======
             name='events',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
@@ -34,10 +18,31 @@ class Migration(migrations.Migration):
                 ('month', models.IntegerField()),
                 ('day', models.IntegerField()),
                 ('year', models.IntegerField()),
+                ('hour', models.IntegerField(null=True)),
+                ('min', models.IntegerField(null=True)),
+                ('notes', models.CharField(max_length=200, null=True)),
+                ('user', models.CharField(max_length=40, null=True)),
             ],
-            options={
-            },
-            bases=(models.Model,),
->>>>>>> 8da517b39a1f3428cbb0342a2083e8ec1e0cda8b
+        ),
+        migrations.CreateModel(
+            name='workouts',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('cal_count', models.IntegerField(default=15000)),
+                ('large_muscle', models.CharField(max_length=10, choices=[(b'CHEST', b'Chest'), (b'THIGH', b'Thighs'), (b'UBACK', b'Upper back'), (b'LBACK', b'Lower back')])),
+                ('small_muscle', models.CharField(max_length=10, choices=[(b'ABS', b'Abdominals'), (b'TRI', b'Triceps'), (b'BIC', b'Biceps'), (b'CAV', b'Calves')])),
+                ('l_ex', models.CharField(max_length=10)),
+                ('s_ex', models.CharField(max_length=10)),
+                ('month', models.IntegerField(null=True)),
+                ('day', models.IntegerField(null=True)),
+                ('year', models.IntegerField(null=True)),
+                ('user', models.CharField(max_length=30, null=True)),
+                ('intensity', models.CharField(max_length=10, null=True, choices=[(b'LIT', b'Light'), (b'MED', b'Medium'), (b'HRD', b'Hard')])),
+                ('workout', models.IntegerField(null=True)),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='workouts',
+            unique_together=set([('month', 'day', 'year')]),
         ),
     ]
